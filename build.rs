b012e65065da2d2b560118fce17e110d39cf2d46
Mickaël Delahaye <mickael.delahaye@gmail.com>
2016-07-03 17:33:22 +0200

    Add build static capstone to build.rs

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ extern crate gcc;
 use std::io::prelude::*;
 use std::env;
 use std::fs::File;
-use std::path::{Path,PathBuf};
+use std::path::Path;
 use std::process::{Command, Stdio};
 
 macro_rules! errln {
@@ -13,14 +13,12 @@ macro_rules! errln {
     }
 }
 
-fn compute_place_holders(out_dir: &str, includes: &[PathBuf]) {
+fn compute_place_holders(out_dir: &str, inc_dir: &str) {
     let out_rs = Path::new(out_dir).join("placeholders.rs");
     let out_exe = Path::new(out_dir).join("compute_placeholders.exe");
 
     let mut config = gcc::Config::new();
-    for inc in includes {
-        config.include(inc);
-    }
+    config.include(inc_dir);
 
     let mut command = config.get_compiler().to_command();
     command.arg("compute_placeholders.c");
@@ -45,17 +43,89 @@ fn compute_place_holders(out_dir: &str, includes: &[PathBuf]) {
     }
 }
 
+// fn find_library() {
+//     let library = pkg_config::Config::new()
+//         .atleast_version("3.0.4")
+//         .probe("capstone");
+//     if let Ok(library) = library {
+//         
+//     }
+// }
 
 fn main() {
 
-    let include_paths =
-        if let Ok(library) = pkg_config::probe_library("capstone") {
-            library.include_paths
-        } else {
-            errln!("cannot find capstone with pkg_config");
-            Vec::new()
-        };
+    // Check if capstone is already cloned
+    if !Path::new("capstone/.git").exists() {
+        let _ = Command::new("git").args(&["submodule", "update", "--init"]).status();
+    }
 
     let out_dir = env::var("OUT_DIR").unwrap();
-    compute_place_holders(&out_dir, &include_paths);
+    compute_place_holders(&out_dir, "capstone/include");
+
+    if !Path::new("capstone/libcapstone.a").exists() {
+        build_capstone();
+    }
+    
+    println!("cargo:rustc-link-search=native=capstone");
+    println!("cargo:rustc-link-lib=static=capstone")
+}
+
+fn build_capstone() {
+
+    let target = env::var("TARGET").unwrap();
+
+    let host = env::var("HOST").unwrap();
+    let host_windows = host.contains("windows");
+
+    // let windows = target.contains("windows");
+    let msvc = target.contains("msvc");
+
+    if msvc && !host_windows {
+        panic!("cannot cross compile to msvc");
+    }
+    
+    if msvc {
+        panic!("msvc build not supported yet");
+    }
+
+    let mut cmd = Command::new("bash");
+    cmd.current_dir("capstone")
+        .arg("make.sh")
+        .env("CAPSTONE_SHARED", "no")
+        .env("CAPSTONE_STATIC", "yes");
+
+    if host == target {
+        // do nothing
+    } else if target == "i686-pc-windows-gnu" {
+        cmd.arg("cross-win32");
+    } else if target == "x86_64-pc-windows-gnu" {
+        cmd.arg("cross-win64");
+    } else if host.starts_with("x86_64-") && target.starts_with("i686-") && host[6..] == target[4..] {
+        // not perfect but hey
+        cmd.arg("nix32");
+    } else {
+        cmd.env("CROSS", target+"-");
+    }
+
+
+    let res = cmd.status();
+    if let Ok(res) = res {
+        if !res.success() {
+            panic!("cannot compile capstone");
+        }
+    } else {
+        panic!("cannot start capstone build");
+    }
+
+
+    // let include_paths =
+    //     if let Ok(library) = pkg_config::probe_library("capstone") {
+    //         library.include_paths
+    //     } else {
+    //         errln!("cannot find capstone with pkg_config");
+    //         Vec::new()
+    //     };
+    //
+    // let out_dir = env::var("OUT_DIR").unwrap();
+    // compute_place_holders(&out_dir, &include_paths);
 }
